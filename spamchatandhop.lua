f72b5867ffe16f98d0e5fe64573cafe112922531
theaug <93984863+vuthaison1308@users.noreply.github.com>
2025-02-13 22:43:50 +0700

    improve hop, add jointeam

--- spamchatandhop.lua
+++ spamchatandhop.lua
@@ -1,4 +1,12 @@
 repeat wait() until game:IsLoaded()
+
+local Player = game.Players.LocalPlayer    
+local Http = game:GetService("HttpService")
+local TPS = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
+
+local _place,_id = game.PlaceId, game.JobId
+local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=10"
 spawn(function()
     while getgenv().AutoText do
         local args = {[1] = getgenv().message,[2] = "All"}
@@ -6,11 +14,15 @@ spawn(function()
         wait(getgenv().delaymessage)
     end
 end)
-
 spawn(function()
     wait(getgenv().HopDelay)
-    if getgenv().Hop then
-        local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/vuthaison1308/vuthaison1308/main/hopserver.lua")()
-        module:Teleport(game.PlaceId)
+    if getgenv().Hop then 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vuthaison1308/nigga/refs/heads/main/bfhop.lua"))()
     end
+end)
+spawn(function()
+    wait(1)
+    local R = game:GetService("ReplicatedStorage");
+    local CommF = R.Remotes.CommF_;
+    CommF:InvokeServer("SetTeam", "Pirates");
 end)
