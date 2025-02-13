116a1d43e315267988037d27d0aab796b8d32cd5
theaug <93984863+vuthaison1308@users.noreply.github.com>
2025-02-13 22:40:49 +0700

    Create bfhop.lua

---
+++ bfhop.lua
@@ -0,0 +1,54 @@
+local ServerFunc = {}
+function ServerFunc.Hop(self)
+	pcall(function()
+		task.delay(20,function()
+			game:Shutdown()
+		end)
+		local ticklon = tick()
+		repeat
+			task.wait()
+		until tick() - ticklon >= 1
+		local function Hop()
+			for r = 1, math.huge do
+				game.Players.LocalPlayer.PlayerGui.ServerBrowser.Frame.Filters.SearchRegion.TextBox.Text = "Singapore"
+				local bP = game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer(r)
+				for k, v in pairs(bP) do
+					if k ~= game.JobId and v["Count"] < 10 then
+						game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport", k)
+					end
+				end
+			end
+			return false
+		end
+		if not LoadedHop then
+			local function bQ(v)
+				if v.Name == "ErrorPrompt" then
+					if v.Visible then
+						if v.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
+							self:Hop()
+							v.Visible = false
+						end
+					end
+					v:GetPropertyChangedSignal("Visible"):Connect(function()
+						if v.Visible then
+							if v.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
+								self:Hop()
+								v.Visible = false
+							end
+						end
+					end)
+				end
+			end
+			for k, v in pairs(game:GetService("CoreGui").RobloxPromptGui.promptOverlay:GetChildren()) do
+				bQ(v)
+			end
+			game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(bQ)
+			LoadedHop = true
+		end
+		while task.wait(0.1) do
+			Hop()
+		end
+	end)
+end
+
+ServerFunc:Hop()
